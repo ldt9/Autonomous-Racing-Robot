ca00106abab71fa1bc1ad2793087f357922ec482
ldt9 <84938803+ldt9@users.noreply.github.com>
2024-04-14 19:09:09 -0400

    here is the base code again

---
+++ Debug/odometry.d
@@ -0,0 +1,90 @@
+# FIXED
+
+odometry.obj: ../odometry.c
+odometry.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdint.h
+odometry.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h
+odometry.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h
+odometry.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_stdint40.h
+odometry.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/stdint.h
+odometry.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h
+odometry.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h
+odometry.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h
+odometry.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_stdint.h
+odometry.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_stdint.h
+odometry.obj: C:/ti/ccs1260/ccs/ccs_base/arm/include/msp.h
+odometry.obj: C:/ti/ccs1260/ccs/ccs_base/arm/include/msp432p401r.h
+odometry.obj: C:/ti/ccs1260/ccs/ccs_base/arm/include/msp_compatibility.h
+odometry.obj: C:/ti/ccs1260/ccs/ccs_base/arm/include/msp432p401r_classic.h
+odometry.obj: C:/ti/ccs1260/ccs/ccs_base/arm/include/CMSIS/core_cm4.h
+odometry.obj: C:/ti/ccs1260/ccs/ccs_base/arm/include/CMSIS/cmsis_compiler.h
+odometry.obj: C:/ti/ccs1260/ccs/ccs_base/arm/include/CMSIS/cmsis_ccs.h
+odometry.obj: C:/ti/ccs1260/ccs/ccs_base/arm/include/system_msp432p401r.h
+odometry.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdio.h
+odometry.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdarg.h
+odometry.obj: ../fixed.h
+odometry.obj: ../Motor.h
+odometry.obj: ../Tachometer.h
+odometry.obj: ../SSD1306.h
+odometry.obj: ../LaunchPad.h
+odometry.obj: ../Bump.h
+odometry.obj: ../Clock.h
+odometry.obj: ../blinker.h
+
+../odometry.c:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdint.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_stdint40.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/stdint.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_stdint.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_stdint.h:
+
+C:/ti/ccs1260/ccs/ccs_base/arm/include/msp.h:
+
+C:/ti/ccs1260/ccs/ccs_base/arm/include/msp432p401r.h:
+
+C:/ti/ccs1260/ccs/ccs_base/arm/include/msp_compatibility.h:
+
+C:/ti/ccs1260/ccs/ccs_base/arm/include/msp432p401r_classic.h:
+
+C:/ti/ccs1260/ccs/ccs_base/arm/include/CMSIS/core_cm4.h:
+
+C:/ti/ccs1260/ccs/ccs_base/arm/include/CMSIS/cmsis_compiler.h:
+
+C:/ti/ccs1260/ccs/ccs_base/arm/include/CMSIS/cmsis_ccs.h:
+
+C:/ti/ccs1260/ccs/ccs_base/arm/include/system_msp432p401r.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdio.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdarg.h:
+
+../fixed.h:
+
+../Motor.h:
+
+../Tachometer.h:
+
+../SSD1306.h:
+
+../LaunchPad.h:
+
+../Bump.h:
+
+../Clock.h:
+
+../blinker.h:
+
