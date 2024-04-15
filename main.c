3ed98276b30fb151d2426c256910b32cac09d432
ldt9 <84938803+ldt9@users.noreply.github.com>
2024-04-14 20:41:31 -0400

    updated wall following

--- main.c
+++ main.c
@@ -77,6 +77,7 @@ uint16_t ActualR;                        // actual rotations per minute measured
 int i_tach = 0;
 int n_tach = 0;
 int stop_tach = 1;                       // The Motors have been stopped
+int wall_follow = 0;                       // wall following enabled
 
 //uint16_t LeftDuty = 3750;                // duty cycle of left wheel (0 to 14,998)
 //uint16_t RightDuty = 3750;               // duty cycle of right wheel (0 to 14,998)
@@ -423,6 +424,190 @@ void SimpleLinkSockEventHandler(SlSockEvent_t *pSock)
  * ASYNCHRONOUS EVENT HANDLERS -- End
  */
 
+/*
+ * Start Wall Following Code
+ */
+
+// assumes track is 500mm
+int32_t Mode=0; // 0 stop, 1 run
+int32_t Error;
+int32_t Ki=1;  // integral controller gain
+int32_t Kp=4;  // proportional controller gain //was 4
+int32_t UR, UL;  // PWM duty 0 to 14,998
+
+#define TOOCLOSE 200 //was 200
+#define DESIRED 250 //was 250
+int32_t SetPoint = 250; // mm //was 250
+int32_t LeftDistance,CenterDistance,RightDistance; // mm
+#define TOOFAR 400 // was 400
+
+#define PWMNOMINAL 5000 // was 2500
+#define SWING 2000 //was 1000
+#define PWMMIN (PWMNOMINAL-SWING)
+#define PWMMAX (PWMNOMINAL+SWING)
+void Controller(void){ // runs at 100 Hz
+  if(Mode){
+    if((LeftDistance>DESIRED)&&(RightDistance>DESIRED)){
+      SetPoint = (LeftDistance+RightDistance)/2;
+    }else{
+      SetPoint = DESIRED;
+    }
+    if(LeftDistance < RightDistance ){
+      Error = LeftDistance-SetPoint;
+    }else {
+      Error = SetPoint-RightDistance;
+    }
+ //   UR = UR + Ki*Error;      // adjust right motor
+    UR = PWMNOMINAL+Kp*Error; // proportional control
+    UL = PWMNOMINAL-Kp*Error; // proportional control
+    if(UR < (PWMNOMINAL-SWING)) UR = PWMNOMINAL-SWING; // 3,000 to 7,000
+    if(UR > (PWMNOMINAL+SWING)) UR = PWMNOMINAL+SWING;
+    if(UL < (PWMNOMINAL-SWING)) UL = PWMNOMINAL-SWING; // 3,000 to 7,000
+    if(UL > (PWMNOMINAL+SWING)) UL = PWMNOMINAL+SWING;
+    Motor_Forward(UL,UR);
+
+  }
+}
+
+void Controller_Right(void){ // runs at 100 Hz
+  if(Mode){
+    if((RightDistance>DESIRED)){
+      SetPoint = (RightDistance)/2;
+    }else{
+      SetPoint = DESIRED;
+    }
+    /*if(LeftDistance < RightDistance ){
+      Error = LeftDistance-SetPoint;
+    }else {
+      Error = SetPoint-RightDistance;
+    }*/
+
+    Error = SetPoint-RightDistance;
+    //UR = UR + Ki*Error;      // adjust right motor
+    UR = PWMNOMINAL+Kp*Error; // proportional control
+    UR = UR + Ki*Error;      // adjust right motor
+    UL = PWMNOMINAL-Kp*Error; // proportional control
+    if(UR < (PWMNOMINAL-SWING)) UR = PWMNOMINAL-SWING; // 3,000 to 7,000
+    if(UR > (PWMNOMINAL+SWING)) UR = PWMNOMINAL+SWING;
+    if(UL < (PWMNOMINAL-SWING)) UL = PWMNOMINAL-SWING; // 3,000 to 7,000
+    if(UL > (PWMNOMINAL+SWING)) UL = PWMNOMINAL+SWING;
+
+    //turns left if the center measurement and right measurement is small enough that we will hit the wall if we don't turn
+    if((RightDistance<250) && (CenterDistance <250)){
+        UL = 0;
+        UR = PWMNOMINAL;
+    }
+
+    Motor_Forward(UL,UR);
+
+  }
+}
+
+void Pause(void){int i;
+  while(Bump_Read()==0x3F){ // wait for release
+    Clock_Delay1ms(200); LaunchPad_Output(0); // off
+    Clock_Delay1ms(200); LaunchPad_Output(1); // red
+  }
+  while(Bump_Read()!=0x3F){// wait for touch
+    Clock_Delay1ms(100); LaunchPad_Output(0); // off
+    Clock_Delay1ms(100); LaunchPad_Output(3); // red/green
+  }
+  while(Bump_Read()==0x3F){ // wait for release
+    Clock_Delay1ms(100); LaunchPad_Output(0); // off
+    Clock_Delay1ms(100); LaunchPad_Output(4); // blue
+  }
+  for(i=1000;i>100;i=i-200){
+    Clock_Delay1ms(i); LaunchPad_Output(0); // off
+    Clock_Delay1ms(i); LaunchPad_Output(2); // green
+  }
+  // restart Jacki
+  UR = UL = PWMNOMINAL;    // reset parameters
+  Mode = 1;
+
+}
+
+// calibrated for 500mm track
+// right is raw sensor data from right sensor
+// return calibrated distance from center of Robot to right wall
+int32_t Right(int32_t right){
+  return  (right*(59*right + 7305) + 2348974)/32768;
+}
+// left is raw sensor data from left sensor
+// return calibrated distance from center of Robot to left wall
+int32_t Left(int32_t left){
+  return (1247*left)/2048 + 22;
+}
+
+void wallFollow(void){ // wallFollow wall following implementation
+  int i = 0;
+  uint32_t channel = 1;
+  DisableInterrupts();
+  Clock_Init48MHz();
+  Bump_Init();
+  LaunchPad_Init(); // built-in switches and LEDs
+  Motor_Stop(); // initialize and stop
+  Mode = 0;
+  I2CB1_Init(30); // baud rate = 12MHz/30=400kHz
+  CLI_Write("OPT3101\n\r");
+  CLI_Write("L=\n\r");
+  CLI_Write("C=\n\r");
+  CLI_Write("R=\n\r");
+  CLI_Write("Wall follow\n\r");
+  CLI_Write("SP=\n\r");
+  CLI_Write("Er=\n\r");
+  CLI_Write("U =\n\r");
+  OPT3101_Init();
+  OPT3101_Setup();
+  OPT3101_CalibrateInternalCrosstalk();
+  OPT3101_ArmInterrupts(&TxChannel, Distances, Amplitudes);
+  TxChannel = 3;
+  OPT3101_StartMeasurementChannel(channel);
+  LPF_Init(100,8);
+  LPF_Init2(100,8);
+  LPF_Init3(100,8);
+  UR = UL = PWMNOMINAL; //initial power
+  Pause();
+  EnableInterrupts();
+  while(1){
+    if(Bump_Read()){ // collision
+      Mode = 0;
+      Motor_Stop();
+      Pause();
+    }
+    if(TxChannel <= 2){ // 0,1,2 means new data
+      if(TxChannel==0){
+        if(Amplitudes[0] > 1000){
+          LeftDistance = FilteredDistances[0] = Left(LPF_Calc(Distances[0]));
+        }else{
+          LeftDistance = FilteredDistances[0] = 500;
+        }
+      }else if(TxChannel==1){
+        if(Amplitudes[1] > 1000){
+          CenterDistance = FilteredDistances[1] = LPF_Calc2(Distances[1]);
+        }else{
+          CenterDistance = FilteredDistances[1] = 500;
+        }
+      }else {
+        if(Amplitudes[2] > 1000){
+          RightDistance = FilteredDistances[2] = Right(LPF_Calc3(Distances[2]));
+        }else{
+          RightDistance = FilteredDistances[2] = 500;
+        }
+      }
+      TxChannel = 3; // 3 means no data
+      channel = (channel+1)%3;
+      OPT3101_StartMeasurementChannel(channel);
+      i = i + 1;
+    }
+    Controller_Right();
+
+    WaitForInterrupt();
+  }
+}
+
+/*
+ * End Wall Follower Code
+ */
 
 /*
  * Application's entry point
@@ -445,6 +630,16 @@ int main(int argc, char** argv)
     // Init Tachometer
     Tachometer_Init();
 
+    // Init Bump Sensors
+    Bump_Init();
+
+    // Init Launch Pad
+    LaunchPad_Init(); // built-in switches and LEDs
+
+    // Set Mode to Stop
+    int i;
+    Mode = 0;
+
     // Init Distance Sensor
     SysTick->LOAD = 0x00FFFFFF;           // maximum reload value
     SysTick->CTRL = 0x00000005;           // enable SysTick with no interrupts
@@ -454,6 +649,12 @@ int main(int argc, char** argv)
     OPT3101_CalibrateInternalCrosstalk();
     OPT3101_StartMeasurementChannel(channel);
     StartTime = SysTick->VAL;
+
+    // Init low pass filters and set power for pwm
+    LPF_Init(100,8);
+    LPF_Init2(100,8);
+    LPF_Init3(100,8);
+    UR = UL = PWMNOMINAL; //initial power
 
     /* Configure command line interface */
     CLI_Configure();
@@ -557,6 +758,9 @@ int main(int argc, char** argv)
     }
     CLI_Write(" Subscribed to uniqueID topic \n\r");
 
+    // wait for bumper signal
+//    Pause();
+
     while(1){
         rc = MQTTYield(&hMQTTClient, 10);
         if (rc != 0) {
@@ -641,8 +845,42 @@ int main(int argc, char** argv)
         // Free the allocated memory for the payload string after publishing the message
         free(payload_str);
 
-        Delay(10);
+//        Delay(10);
 
+        if(Bump_Read()!=0x3F){ // collision
+          Mode = 0;
+          stop_tach = 1;
+          Motor_Stop();
+          Pause();
+        }
+        if (wall_follow){
+            if(TxChannel <= 2){ // 0,1,2 means new data
+              if(TxChannel==0){
+                if(Amplitudes[0] > 1000){
+                  LeftDistance = FilteredDistances[0] = Left(LPF_Calc(Distances[0]));
+                }else{
+                  LeftDistance = FilteredDistances[0] = 500;
+                }
+              }else if(TxChannel==1){
+                if(Amplitudes[1] > 1000){
+                  CenterDistance = FilteredDistances[1] = LPF_Calc2(Distances[1]);
+                }else{
+                  CenterDistance = FilteredDistances[1] = 500;
+                }
+              }else {
+                if(Amplitudes[2] > 1000){
+                  RightDistance = FilteredDistances[2] = Right(LPF_Calc3(Distances[2]));
+                }else{
+                  RightDistance = FilteredDistances[2] = 500;
+                }
+              }
+              TxChannel = 3; // 3 means no data
+              channel = (channel+1)%3;
+              OPT3101_StartMeasurementChannel(channel);
+              i = i + 1;
+            }
+            Controller_Right();
+        }
     }
 }
 
@@ -697,10 +935,11 @@ static void messageArrived(MessageData* data) {
     CLI_Write("\n\r");
 
     if (strcmp(tok, "go") == 0) {
-        Motor_Forward(7500,0);
+        wall_follow = 1;
         stop_tach = 0;
     } else if (strcmp(buf, "stop") == 0) {
         Motor_Stop();
+        wall_follow = 0;
         stop_tach = 1;
     } else {
         CLI_Write(" Unacceptable Input");
@@ -953,195 +1192,4 @@ static void displayBanner()
     CLI_Write("\n\r*******************************************************************************\n\r");
 }
 
-/*
- * This code below guides the robot as a specified distance from the wall.
- */
-
-// assumes track is 500mm
-int32_t Mode=0; // 0 stop, 1 run
-int32_t Error;
-int32_t Ki=1;  // integral controller gain
-int32_t Kp=4;  // proportional controller gain //was 4
-int32_t UR, UL;  // PWM duty 0 to 14,998
-
-#define TOOCLOSE 200 //was 200
-#define DESIRED 250 //was 250
-int32_t SetPoint = 250; // mm //was 250
-int32_t LeftDistance,CenterDistance,RightDistance; // mm
-#define TOOFAR 400 // was 400
-
-#define PWMNOMINAL 5000 // was 2500
-#define SWING 2000 //was 1000
-#define PWMMIN (PWMNOMINAL-SWING)
-#define PWMMAX (PWMNOMINAL+SWING)
-void Controller(void){ // runs at 100 Hz
-  if(Mode){
-    if((LeftDistance>DESIRED)&&(RightDistance>DESIRED)){
-      SetPoint = (LeftDistance+RightDistance)/2;
-    }else{
-      SetPoint = DESIRED;
-    }
-    if(LeftDistance < RightDistance ){
-      Error = LeftDistance-SetPoint;
-    }else {
-      Error = SetPoint-RightDistance;
-    }
- //   UR = UR + Ki*Error;      // adjust right motor
-    UR = PWMNOMINAL+Kp*Error; // proportional control
-    UL = PWMNOMINAL-Kp*Error; // proportional control
-    if(UR < (PWMNOMINAL-SWING)) UR = PWMNOMINAL-SWING; // 3,000 to 7,000
-    if(UR > (PWMNOMINAL+SWING)) UR = PWMNOMINAL+SWING;
-    if(UL < (PWMNOMINAL-SWING)) UL = PWMNOMINAL-SWING; // 3,000 to 7,000
-    if(UL > (PWMNOMINAL+SWING)) UL = PWMNOMINAL+SWING;
-    Motor_Forward(UL,UR);
-
-  }
-}
-
-void Controller_Right(void){ // runs at 100 Hz
-  if(Mode){
-    if((RightDistance>DESIRED)){
-      SetPoint = (RightDistance)/2;
-    }else{
-      SetPoint = DESIRED;
-    }
-    /*if(LeftDistance < RightDistance ){
-      Error = LeftDistance-SetPoint;
-    }else {
-      Error = SetPoint-RightDistance;
-    }*/
-
-    Error = SetPoint-RightDistance;
-    //UR = UR + Ki*Error;      // adjust right motor
-    UR = PWMNOMINAL+Kp*Error; // proportional control
-    UR = UR + Ki*Error;      // adjust right motor
-    UL = PWMNOMINAL-Kp*Error; // proportional control
-    if(UR < (PWMNOMINAL-SWING)) UR = PWMNOMINAL-SWING; // 3,000 to 7,000
-    if(UR > (PWMNOMINAL+SWING)) UR = PWMNOMINAL+SWING;
-    if(UL < (PWMNOMINAL-SWING)) UL = PWMNOMINAL-SWING; // 3,000 to 7,000
-    if(UL > (PWMNOMINAL+SWING)) UL = PWMNOMINAL+SWING;
-
-    //turns left if the center measurement and right measurement is small enough that we will hit the wall if we don't turn
-    if((RightDistance<250) && (CenterDistance <250)){
-        UL = 0;
-        UR = PWMNOMINAL;
-    }
-
-    Motor_Forward(UL,UR);
-
-  }
-}
-
-void Pause(void){int i;
-  while(Bump_Read()){ // wait for release
-    Clock_Delay1ms(200); LaunchPad_Output(0); // off
-    Clock_Delay1ms(200); LaunchPad_Output(1); // red
-  }
-  while(Bump_Read()==0){// wait for touch
-    Clock_Delay1ms(100); LaunchPad_Output(0); // off
-    Clock_Delay1ms(100); LaunchPad_Output(3); // red/green
-  }
-  while(Bump_Read()){ // wait for release
-    Clock_Delay1ms(100); LaunchPad_Output(0); // off
-    Clock_Delay1ms(100); LaunchPad_Output(4); // blue
-  }
-  for(i=1000;i>100;i=i-200){
-    Clock_Delay1ms(i); LaunchPad_Output(0); // off
-    Clock_Delay1ms(i); LaunchPad_Output(2); // green
-  }
-  // restart Jacki
-  UR = UL = PWMNOMINAL;    // reset parameters
-  Mode = 1;
-
-}
-
-// calibrated for 500mm track
-// right is raw sensor data from right sensor
-// return calibrated distance from center of Robot to right wall
-int32_t Right(int32_t right){
-  return  (right*(59*right + 7305) + 2348974)/32768;
-}
-// left is raw sensor data from left sensor
-// return calibrated distance from center of Robot to left wall
-int32_t Left(int32_t left){
-  return (1247*left)/2048 + 22;
-}
-
-void wallFollow(void){ // wallFollow wall following implementation
-  int i = 0;
-  uint32_t channel = 1;
-  DisableInterrupts();
-  Clock_Init48MHz();
-  Bump_Init();
-  LaunchPad_Init(); // built-in switches and LEDs
-  Motor_Stop(); // initialize and stop
-  Mode = 0;
-  I2CB1_Init(30); // baud rate = 12MHz/30=400kHz
-  CLI_Write("OPT3101\n\r");
-  CLI_Write("L=\n\r");
-  CLI_Write("C=\n\r");
-  CLI_Write("R=\n\r");
-  CLI_Write("Wall follow\n\r");
-  CLI_Write("SP=\n\r");
-  CLI_Write("Er=\n\r");
-  CLI_Write("U =\n\r");
-  OPT3101_Init();
-  OPT3101_Setup();
-  OPT3101_CalibrateInternalCrosstalk();
-  OPT3101_ArmInterrupts(&TxChannel, Distances, Amplitudes);
-  TxChannel = 3;
-  OPT3101_StartMeasurementChannel(channel);
-  LPF_Init(100,8);
-  LPF_Init2(100,8);
-  LPF_Init3(100,8);
-  UR = UL = PWMNOMINAL; //initial power
-  Pause();
-  EnableInterrupts();
-  while(1){
-    if(Bump_Read()){ // collision
-      Mode = 0;
-      Motor_Stop();
-      Pause();
-    }
-    if(TxChannel <= 2){ // 0,1,2 means new data
-      if(TxChannel==0){
-        if(Amplitudes[0] > 1000){
-          LeftDistance = FilteredDistances[0] = Left(LPF_Calc(Distances[0]));
-        }else{
-          LeftDistance = FilteredDistances[0] = 500;
-        }
-      }else if(TxChannel==1){
-        if(Amplitudes[1] > 1000){
-          CenterDistance = FilteredDistances[1] = LPF_Calc2(Distances[1]);
-        }else{
-          CenterDistance = FilteredDistances[1] = 500;
-        }
-      }else {
-        if(Amplitudes[2] > 1000){
-          RightDistance = FilteredDistances[2] = Right(LPF_Calc3(Distances[2]));
-        }else{
-          RightDistance = FilteredDistances[2] = 500;
-        }
-      }
-//      SetCursor(2, TxChannel+1);
-//      OutUDec(FilteredDistances[TxChannel]); OutChar(','); OutUDec(Amplitudes[TxChannel]);
-      TxChannel = 3; // 3 means no data
-      channel = (channel+1)%3;
-      OPT3101_StartMeasurementChannel(channel);
-      i = i + 1;
-    }
-    Controller_Right();
-//    if(i >= 100){
-//      i = 0;
-//      SetCursor(3, 5);
-//      OutUDec(SetPoint);
-//      SetCursor(3, 6);
-//      OutSDec(Error);
-//      SetCursor(3, 7);
-//      OutUDec(UL); OutChar(','); OutUDec(UR);
-//    }
-
-    WaitForInterrupt();
-  }
-}
 
